c224f7967499181a86eca181d757c760dfbc6db7
Luis Eduardo Sagnay Pilamunga <sagnayluis99@gmail.com>
2025-06-30 17:38:35 -0500

    algoritmos

--- Algorithms/Algorithms/Views/FrmBSpline.Designer.cs
+++ Algorithms/Algorithms/Views/FrmBSpline.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Algorithms.Views
 {
-    partial class FrmCurves
+    partial class FrmBSpline
     {
         /// <summary>
         /// Required designer variable.
@@ -28,43 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.rbBezier = new System.Windows.Forms.RadioButton();
-            this.rbBSpines = new System.Windows.Forms.RadioButton();
             this.lblTitulo = new System.Windows.Forms.Label();
             this.btnBack = new System.Windows.Forms.Button();
             this.btnReset = new System.Windows.Forms.Button();
             this.btnCalculate = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
-            this.lblInputs = new System.Windows.Forms.Label();
             this.picCanvas = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
             this.SuspendLayout();
-            // 
-            // rbBezier
-            // 
-            this.rbBezier.AutoSize = true;
-            this.rbBezier.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rbBezier.Location = new System.Drawing.Point(59, 190);
-            this.rbBezier.Name = "rbBezier";
-            this.rbBezier.Size = new System.Drawing.Size(81, 26);
-            this.rbBezier.TabIndex = 107;
-            this.rbBezier.TabStop = true;
-            this.rbBezier.Text = "Bezier";
-            this.rbBezier.UseVisualStyleBackColor = true;
-            this.rbBezier.CheckedChanged += new System.EventHandler(this.rb_CheckedChanged);
-            // 
-            // rbBSpines
-            // 
-            this.rbBSpines.AutoSize = true;
-            this.rbBSpines.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rbBSpines.Location = new System.Drawing.Point(59, 236);
-            this.rbBSpines.Name = "rbBSpines";
-            this.rbBSpines.Size = new System.Drawing.Size(108, 26);
-            this.rbBSpines.TabIndex = 106;
-            this.rbBSpines.TabStop = true;
-            this.rbBSpines.Text = "B-Splines";
-            this.rbBSpines.UseVisualStyleBackColor = true;
-            this.rbBSpines.CheckedChanged += new System.EventHandler(this.rb_CheckedChanged);
             // 
             // lblTitulo
             // 
@@ -72,12 +42,12 @@
             this.lblTitulo.BackColor = System.Drawing.Color.SteelBlue;
             this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
             this.lblTitulo.ForeColor = System.Drawing.SystemColors.Control;
-            this.lblTitulo.Location = new System.Drawing.Point(-1, 33);
+            this.lblTitulo.Location = new System.Drawing.Point(-1, 28);
             this.lblTitulo.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.lblTitulo.Name = "lblTitulo";
             this.lblTitulo.Padding = new System.Windows.Forms.Padding(390, 8, 390, 8);
             this.lblTitulo.Size = new System.Drawing.Size(904, 47);
-            this.lblTitulo.TabIndex = 104;
+            this.lblTitulo.TabIndex = 109;
             this.lblTitulo.Text = "Flood Fill";
             // 
             // btnBack
@@ -85,11 +55,11 @@
             this.btnBack.BackColor = System.Drawing.Color.SteelBlue;
             this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.btnBack.ForeColor = System.Drawing.Color.White;
-            this.btnBack.Location = new System.Drawing.Point(181, 461);
+            this.btnBack.Location = new System.Drawing.Point(562, 482);
             this.btnBack.Margin = new System.Windows.Forms.Padding(4);
             this.btnBack.Name = "btnBack";
             this.btnBack.Size = new System.Drawing.Size(95, 43);
-            this.btnBack.TabIndex = 103;
+            this.btnBack.TabIndex = 108;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = false;
             // 
@@ -98,77 +68,49 @@
             this.btnReset.BackColor = System.Drawing.Color.SteelBlue;
             this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.btnReset.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnReset.Location = new System.Drawing.Point(59, 461);
+            this.btnReset.Location = new System.Drawing.Point(406, 482);
             this.btnReset.Margin = new System.Windows.Forms.Padding(4);
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(95, 43);
-            this.btnReset.TabIndex = 102;
+            this.btnReset.TabIndex = 107;
             this.btnReset.Text = "Reset";
             this.btnReset.UseVisualStyleBackColor = false;
-            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
             // btnCalculate
             // 
             this.btnCalculate.BackColor = System.Drawing.Color.SteelBlue;
             this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.btnCalculate.ForeColor = System.Drawing.Color.White;
-            this.btnCalculate.Location = new System.Drawing.Point(59, 399);
+            this.btnCalculate.Location = new System.Drawing.Point(246, 482);
             this.btnCalculate.Margin = new System.Windows.Forms.Padding(4);
             this.btnCalculate.Name = "btnCalculate";
-            this.btnCalculate.Size = new System.Drawing.Size(217, 43);
-            this.btnCalculate.TabIndex = 101;
-            this.btnCalculate.Text = "Draw Curve";
+            this.btnCalculate.Size = new System.Drawing.Size(95, 43);
+            this.btnCalculate.TabIndex = 106;
+            this.btnCalculate.Text = "Draw ";
             this.btnCalculate.UseVisualStyleBackColor = false;
-            this.btnCalculate.Click += new System.EventHandler(this.btnDraw_Click);
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(143, 141);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(167, 16);
-            this.label5.TabIndex = 95;
-            this.label5.Text = "----------------------------------------";
-            // 
-            // lblInputs
-            // 
-            this.lblInputs.AutoSize = true;
-            this.lblInputs.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.lblInputs.Location = new System.Drawing.Point(54, 135);
-            this.lblInputs.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblInputs.Name = "lblInputs";
-            this.lblInputs.Size = new System.Drawing.Size(81, 25);
-            this.lblInputs.TabIndex = 94;
-            this.lblInputs.Text = "Choose";
             // 
             // picCanvas
             // 
             this.picCanvas.BackColor = System.Drawing.Color.AliceBlue;
-            this.picCanvas.Location = new System.Drawing.Point(357, 141);
+            this.picCanvas.Location = new System.Drawing.Point(52, 117);
             this.picCanvas.Margin = new System.Windows.Forms.Padding(4);
             this.picCanvas.Name = "picCanvas";
-            this.picCanvas.Size = new System.Drawing.Size(490, 363);
-            this.picCanvas.TabIndex = 93;
+            this.picCanvas.Size = new System.Drawing.Size(798, 343);
+            this.picCanvas.TabIndex = 105;
             this.picCanvas.TabStop = false;
             // 
-            // FrmCurves
+            // FrmBSpline
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(902, 553);
-            this.Controls.Add(this.rbBezier);
-            this.Controls.Add(this.rbBSpines);
             this.Controls.Add(this.lblTitulo);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.btnCalculate);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.lblInputs);
             this.Controls.Add(this.picCanvas);
-            this.Name = "FrmCurves";
-            this.Text = "FrmCurves";
-            this.Click += new System.EventHandler(this.FrmCurves_Load);
+            this.Name = "FrmBSpline";
+            this.Text = "FrmBSplines";
             ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -176,14 +118,11 @@
         }
 
         #endregion
-        private System.Windows.Forms.RadioButton rbBezier;
-        private System.Windows.Forms.RadioButton rbBSpines;
+
         private System.Windows.Forms.Label lblTitulo;
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Button btnReset;
         private System.Windows.Forms.Button btnCalculate;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label lblInputs;
         private System.Windows.Forms.PictureBox picCanvas;
     }
 }
---
+++ Algorithms/Algorithms/Views/FrmBSpline.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Algorithms.Algorithm.Curves;
+using Algorithms.Domain.Abstract;
+
+namespace Algorithms.Views
+{
+    public partial class FrmBSpline : Form
+    {
+        private CurveAlgorithm curva = new BSplineCurve();
+
+        public FrmBSpline()
+        {
+            InitializeComponent();
+            picCanvas.MouseClick += PicCanvas_MouseClick;
+        }
+
+        private void PicCanvas_MouseClick(object sender, MouseEventArgs e)
+        {
+            curva.AgregarPunto(e.Location);
+            Redibujar();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            curva.Resetear();
+            Redibujar();
+        }
+
+        private void Redibujar()
+        {
+            Bitmap bmp = new Bitmap(picCanvas.Width, picCanvas.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+
+                foreach (var p in curva.PuntosControl)
+                    g.FillEllipse(Brushes.Black, p.X - 3, p.Y - 3, 6, 6);
+
+                if (curva.PuntosControl.Count > 1)
+                    g.DrawLines(Pens.Gray, curva.PuntosControl.ToArray());
+
+                if (curva.CurvaGenerada.Count > 1)
+                    g.DrawLines(Pens.Red, curva.CurvaGenerada.ToArray());
+            }
+
+            picCanvas.Image?.Dispose();
+            picCanvas.Image = bmp;
+        }
+    }
+
+}
